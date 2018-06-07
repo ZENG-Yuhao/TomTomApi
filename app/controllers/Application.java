8da076dda6f1a56535a3f4d443b5944c2289ca1b
Yuhao ZENG (Enzo) <yuhao.zeng@coaxys.com>
2018-06-07 16:13:13 +0200

    ...

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,6 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import java.util.*;
-
-import models.*;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
@@ -13,4 +8,7 @@ public class Application extends Controller {
         render();
     }
 
+    public static void tstest(){
+        render();
+    }
 }
